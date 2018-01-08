a682470420936e77995173a349af91479d425d92
Skaha_AM <oneakun08@gmail.com>
2018-01-08 21:17:31 +0700

    Makasih Bapak Arifin
    Signed-off-by: Skaha_AM <oneakun08@gmail.com>

---
+++ app/src/main/java/com/harjay_reza/susu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.harjay_reza.susu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
